9a06b8b26a6fc8176724fd73a5f0f74bf1020bee
Artem <64410376+Artiss90@users.noreply.github.com>
2021-04-11 01:38:33 +0300

    complete

--- contacts.js
+++ contacts.js
@@ -1,9 +1,13 @@
 const fs = require("fs").promises;
 const path = require("path");
+const Joi = require("joi");
+const uniqid = require("uniqid");
 
 const contactsPath = path.join(__dirname, "db", "contacts.json");
 
 // TODO: задокументировать каждую функцию
+// ? асинхронная функция вызова всего списка путь к которому указан в аргументе filesPath
+// * пример использования: listContacts(contactsPath).then(console.table)
 async function listContacts(filesPath) {
   const list = await fs.readFile(filesPath, "utf-8", (err, data) => {
     if (err) {
@@ -15,36 +19,64 @@ async function listContacts(filesPath) {
   return JSON.parse(list);
 }
 
+// ? асинхронная функция вызова по ID элемента из списка указанного в переменной list
+// ? аргумент ID передается в строковом типе
+// * пример использования: getContactById('bmuwatsknc8zxfi').then(console.table)
 async function getContactById(contactId) {
   const list = await listContacts(contactsPath);
   const itemId = await list.find(({ id }) => id === contactId);
   return itemId;
 }
 
+// ? асинхронная функция удаления по ID элемента из списка указанного в переменной list
+// ? аргумент ID передается в строковом типе
+// * пример использования: // removeContact('bmuwatsknc8zxfi')
 async function removeContact(contactId) {
   const list = await listContacts(contactsPath);
   const listAfterRemove = await list.filter(({ id }) => id !== contactId);
-  const listAfterRemoveJSON = JSON.stringify(listAfterRemove);
+  const listAfterRemoveJSON = await JSON.stringify(listAfterRemove);
   const writeList = await fs.writeFile(
     contactsPath,
     listAfterRemoveJSON,
-    "utf-8",
     (err) => {
       if (err) {
         console.error(err.message);
         return;
+      } else {
+        console.log("The file has been remove!");
       }
     }
   );
   return writeList;
 }
 
-function addContact(name, email, phone) {
-  // ...твой код
+// ? асинхронная функция добавления элемента в список указанный в переменной list
+// ? значение ID в объекте создаваемого в переменной objId генерируется в виде строки
+// ? у функции должен передаваться как минимум 1 из аргументов, что обусловлено проверкой в переменной schemaAdd
+// * пример использования: addContact("Mango", "mango@gmail.com", "999-11-99")
+async function addContact(name, email, phone) {
+  const schemaAdd = Joi.object({
+    name: Joi.string(),
+    email: Joi.string(),
+    phone: Joi.string(),
+  }).min(1);
+  const objId = { id: uniqid() };
+  try {
+    const { value } = schemaAdd.validate({
+      name: name,
+      email: email,
+      phone: phone,
+    });
+    const objJoinJSON = { ...objId, ...value };
+    const list = await listContacts(contactsPath);
+    const resultListJSON = JSON.stringify([...list, objJoinJSON]);
+    fs.writeFile(contactsPath, resultListJSON, (err) => {
+      if (err) throw err;
+      console.log("The contact was appended to file!");
+    });
+  } catch (err) {
+    (err) => console.log(err.message);
+  }
 }
 
-// listContacts(contactsPath).then(console.table);
-// getContactById(2).then(console.table);
-removeContact(2).then(console.table);
-
-// module.exports = contactsPath;
+module.exports = { listContacts, getContactById, removeContact, addContact };
--- index.js
+++ index.js
@@ -1,1 +1,49 @@
-console.log("Hello");
+const path = require("path");
+const contactsPath = path.join(__dirname, "db", "contacts.json");
+const {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+} = require("./contacts");
+const { Command } = require("commander");
+const program = new Command();
+program
+  .option("-a, --action <type>", "choose action")
+  .option("-i, --id <type>", "user id")
+  .option("-n, --name <type>", "user name")
+  .option("-e, --email <type>", "user email")
+  .option("-p, --phone <type>", "user phone");
+
+program.parse(process.argv);
+
+const argv = program.opts();
+//? проверка вводимых в консоль значений: console.log(argv)
+// TODO: отрефакторил
+function invokeAction({ action, id, name, email, phone }) {
+  switch (action) {
+    case "list":
+      listContacts(contactsPath).then(console.table);
+      break;
+
+    case "get":
+      // ... id
+      getContactById(id).then(console.table);
+      break;
+
+    case "add":
+      // ... name email phone
+      addContact(name, email, phone);
+      break;
+
+    case "remove":
+      // ... id
+      removeContact(id);
+      break;
+
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+}
+
+invokeAction(argv);
